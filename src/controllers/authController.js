16a48f7eba248f78bc183c6899977aecb672d02c
[teamdeathmatch] <[ozlemkayasaroglu@gmail.com]>
2023-12-28 21:16:55 +0300

    auth ok

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,12 +1,13 @@
-import express, { response } from 'express';
+import express, { response } from "express";
 import dotenv from "dotenv";
 import fs from "fs";
 import path, { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
 import bcrypt from "bcryptjs";
-
+import { serialize } from "cookie";
 
 dotenv.config();
+
 const router = express.Router();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,61 +15,75 @@ const userDataPath = path.join(__dirname, "..", "data", "users.json");
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-router.post("/login", async ( request, response) => {
-const {username, password} = request.body;
+router.post("/login", async (request, response) => {
+  const { username, password } = request.body;
 
-try{
-const users= await getUsers();
-const user= users.find((user) => user.username === username);
+  try {
+    if (!JWT_SECRET) {
+      throw new Error("JWT değişkenlerle tanımlanmamış");
+    }
 
-if(!user){
-  throw new Error("kullanıcı bulunamadı");
-}
+    const users = await getUsers();
+    const user = users.find((user) => user.username === username);
 
-if (!JWT_SECRET) {
-  throw new Error ("JWT değişkenlerle tanımlanmamış")
-}
+    if (!user) {
+      throw new Error("kullanıcı bulunamadı");
+    }
 
-if(user && bcrypt.compareSync(password, user.password)){
-  return response.json({message:"kullanıcı doğrulandı"})
-}
-response.json();
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = jwt.sign({ id: user.id }, JWT_SECRET, {
+        expiresIn: "30d",
+      });
 
-}catch{
-  response.status(500).json({
-    message: "kullanıcı giriş hatası"
-  })
-}
-})
+      const serializedToken = serialize("echommerceToken", token, {
+        httpOnly: true,
+        maxAge: 60 * 6 * 24 * 30,
+        secure: false,
+        path: "/",
+      });
 
+      response.setHeader("Set-Cookie", serializedToken);
+      return response.json({
+        message: "kullanıcı doğrulandı",
+        token: token,
+      });
+
+    } else {
+      throw new Error("şifre hatalı");
+    }
+    
+  } catch (e) {
+    response.status(500).json({
+      message: e.message,
+    });
+  }
+});
 
 const getUsers = () => {
   return new Promise((resolve, reject) => {
-    fs.readFile(userDataPath, "utf8", (err,data) => {
+    fs.readFile(userDataPath, "utf8", (err, data) => {
       err ? reject(data) : resolve(JSON.parse(data));
     });
   });
 };
 
 router.get("/logout", (req, res) => {
-  const {cookies} = req;
+  const { cookies } = req;
   const jwt = cookies?.JWTToken;
-if(!jwt) {
-  return res.json({
+  if (!jwt) {
+    return res.json({
       message: "already logged out.",
+    });
+  }
+
+  const serialized = serialize("JWTToken", "", {
+    httpOnly: true,
+    secure: false,
+    maxAge: -1,
+    path: "/",
   });
-}
-
-
-const serialized =serialize("JWTToken", "", {
-  httpOnly: true,
-  secure: false,
-  maxAge: -1,
-  path: "/",
-})
-res.setHeader("setCookie", serialized);
-res.status(200).json({message:"successfully logged out"});
-})
-
+  res.setHeader("setCookie", serialized);
+  res.status(200).json({ message: "successfully logged out" });
+});
 
 export default router;
